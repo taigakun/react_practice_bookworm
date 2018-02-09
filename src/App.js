a5ce6f43ae9c223106d15b872c3be32f74a1942b
Taiga Matsumoto <matsumoto26761192@gmail.com>
2018-02-09 13:03:36 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Route} from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+
+const App = () => (<div className="ui container">
+  <Route path="/" exact="exact" component={HomePage}/>
+  <Route path="/login" exact="exact" component={LoginPage}/>
+</div>);
+
+export default App;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import LoginForm from "../forms/LoginForm";
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+class LoginPage extends React.Component {
+  submit = (data) => this.props.login(data).then(() => this.props.history.push("/"));
+
+  render() {
+    return (<div>
+      <h1>Login Page</h1>
+
+      <LoginForm submit={this.submit}/>
+    </div>);
+  }
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({push: PropTypes.func.isRequired}).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect()(LoginPage);
